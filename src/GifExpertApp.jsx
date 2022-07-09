113b212532e325d9d89e89034d8502798a63c4f5
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-09 11:45:05 +0200

    WIP

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Test", "Test2"]);
   return (
     <>
       <h1>GifExpertApp</h1>
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 };
