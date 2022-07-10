baf2541e29ecdad6d12e80c4b5506b1476cf6bfa
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-10 12:23:16 +0200

    WIP

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["Test", "Test2"]);
+
+  const onAddCategory = () => {
+    setCategories(["Push", ...categories]);
+  };
   return (
     <>
       <h1>GifExpertApp</h1>
+      <button onClick={onAddCategory}>Add</button>
       <ol>
         {categories.map((category) => (
           <li key={category}>{category}</li>
