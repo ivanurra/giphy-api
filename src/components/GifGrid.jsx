1623035c211f6970577af077cc8669325381054a
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-14 17:20:21 +0200

    WIP

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,13 @@
-export const GifGrid = ({category}) => {
+export const GifGrid = ({ category }) => {
+  const getGifs = async () => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&api_key=${process.env.REACT_APP_API_KEY}`;
+
+    const resp = await fetch(url);
+    console.log(resp);
+  };
+
+  getGifs();
+
   return (
     <>
       <h2>{category}</h2>
