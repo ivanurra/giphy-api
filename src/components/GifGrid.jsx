99635b35752ad4514bf21350c640c0055cc7c2d4
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-17 11:58:33 +0200

    WIP

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,7 @@
+import { getGifs } from "../helpers/getGifs";
+
 export const GifGrid = ({ category }) => {
-  const getGifs = async () => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&api_key=${
-      import.meta.env.VITE_SOME_KEY
-    }`;
-
-    const resp = await fetch(url);
-    console.log(resp);
-  };
-
-  getGifs();
+  getGifs(category);
 
   return (
     <>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&api_key=${
+      import.meta.env.VITE_SOME_KEY
+    }&limit=10`;
+  
+    const resp = await fetch(url);
+  
+    const { data } = await resp.json();
+  
+    const gifs = data.map((img) => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    }));
+  
+    console.log(gifs);
+    return gifs;
+  };
