82c927cabcaa25ef179242a122da470c5d19f3dc
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-17 13:19:21 +0200

    WIP

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
   useEffect(() => {
-    getGifs(category);
+    getImages();
   }, []);
 
   return (
     <>
       <h2>{category}</h2>
+
+      <ol>
+        {images.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
